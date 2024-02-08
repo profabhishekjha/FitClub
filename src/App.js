53598f601294e18ef50df4bf7c8f0a973e91c4c4
profabhishekjha <jhaabhishek910@gmail.com>
2024-02-08 11:00:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import "./App.css";
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <span style={{ color: "white" }}>Subscribe 3 Peak Journals.</span>
     </div>
   );
 }
